bbcc42060bb76b6d59b11470dabc22749600d914
Zuwaynib <zainabadeoje@gmail.com>
2023-08-04 12:33:40 -0700

    base 16 refactored

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,15 +7,12 @@
  */
 int main(void)
 {
+	char base[16] = "0123456789abcdef";
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 0; i < 16; i++)
 	{
-		putchar(i);
-	}
-	for (i = 97; i < 103; i++)
-	{
-		putchar(i);
+		putchar(base[i]);
 	}
 	putchar('\n');
 	return (0);
